14db73e5f0bfa4d7190d2b492d5e584111cca734
trentduncan <trent.dunc@gmail.com>
2018-03-20 14:08:09 -0700

    finished routes using mongoose

--- routes/notes.js
+++ routes/notes.js
@@ -29,33 +29,64 @@ router.get('/notes', (req, res, next) => {
 
 /* ========== GET/READ A SINGLE ITEM ========== */
 router.get('/notes/:id', (req, res, next) => {
+  const {id} = req.params;
 
-  console.log('Get a Note');
-  res.json({ id: 2 });
-
+  return Note.findById(id)
+    .then(result => {
+      res.json(result);
+    })
+    .catch(next);
 });
 
 /* ========== POST/CREATE AN ITEM ========== */
 router.post('/notes', (req, res, next) => {
-
-  console.log('Create a Note');
-  res.location('path/to/new/document').status(201).json({ id: 2 });
-
+  const { title, content } = req.body;
+  
+  const newItem = {title, content};
+  /***** Never trust users - validate input *****/
+  if (!newItem.title) {
+    const err = new Error('Missing `title` in request body');
+    err.status = 400;
+    return next(err);
+  }
+  return Note.create(newItem)
+    .then(note => {
+      res.location(`${req.originalUrl}/${note.id}`).status(201).json(note);
+    })
+    .catch(next);
 });
 
 /* ========== PUT/UPDATE A SINGLE ITEM ========== */
 router.put('/notes/:id', (req, res, next) => {
+  const updateObj = {};
+  const updateableFields = ['title', 'content'];
 
-  console.log('Update a Note');
-  res.json({ id: 2 });
+  updateableFields.forEach(field => {
+    if (field in req.body) {
+      updateObj[field] = req.body[field];
+    }
+  });
 
+  /***** Never trust users - validate input *****/
+  if (!updateObj.title) {
+    const err = new Error('Missing `title` in request body');
+    err.status = 400;
+    return next(err);
+  }
+  return Note.findByIdAndUpdate(req.params.id, {$set : updateObj}, {new : true})
+    .then(note => {
+      res.json(note);
+    })
+    .catch(next);
 });
 
 /* ========== DELETE/REMOVE A SINGLE ITEM ========== */
 router.delete('/notes/:id', (req, res, next) => {
-
-  console.log('Delete a Note');
-  res.status(204).end();
+  return Note.findByIdAndRemove(req.params.id)
+    .then(() => {
+      res.status(204).end();
+    })
+    .catch(next);
 
 });
 
--- scratch/queries.js
+++ scratch/queries.js
@@ -6,30 +6,38 @@ const { MONGODB_URI } = require('../config');
 
 const Note = require('../models/note');
 
+// mongoose.connect(MONGODB_URI)
+//   .then(() => {
+//     const searchTerm = 'lady gaga';
+//     let filter = {};
+
+//     if (searchTerm) {
+//       const re = new RegExp(searchTerm, 'i');
+//       filter.title = { $regex: re };
+//     }
+
+//     return Note.find(filter)
+//       .sort('created')
+//       .then(results => {
+//         console.log(results);
+//       })
+//       .catch(console.error);
+//   })
+//   .then(() => {
+//     return mongoose.disconnect()
+//       .then(() => {
+//         console.info('Disconnected');
+//       });
+//   })
+//   .catch(err => {
+//     console.error(`ERROR: ${err.message}`);
+//     console.error(err);
+//   });
+
 mongoose.connect(MONGODB_URI)
   .then(() => {
-    const searchTerm = 'lady gaga';
-    let filter = {};
-
-    if (searchTerm) {
-      const re = new RegExp(searchTerm, 'i');
-      filter.title = { $regex: re };
-    }
-
-    return Note.find(filter)
-      .sort('created')
-      .then(results => {
-        console.log(results);
-      })
-      .catch(console.error);
-  })
-  .then(() => {
-    return mongoose.disconnect()
-      .then(() => {
-        console.info('Disconnected');
+    return Note.findByIdAndUpdate('5ab170b6272376c8c5d7a79f' , {$set : {title:'new bgfgdhghfghgfitle', content: 'newhgfhfghfhgh ccontent'}}, {new: true})
+      .then(note => {
+        console.log(note);
       });
-  })
-  .catch(err => {
-    console.error(`ERROR: ${err.message}`);
-    console.error(err);
   });
